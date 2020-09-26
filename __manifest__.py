5d08a3ed15b9718408e7a8f94ff7ecfb93c9ad26
POTCHONA Justin <potchjust@gmail.com>
2020-09-26 16:00:29 +0000

    :construction: init
    continu module basic initialisation

--- __manifest__.py
+++ __manifest__.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "library_app",
+    'name': "Library Managment system",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': "A custom library management library for learning odoo development addons",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "MAONO",
+    'website': "http://www.new.maono.co",
+    'application':True,
+    'installable':True,
+    'auto-install':True,
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -29,7 +30,5 @@
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'demo': [],
 }
